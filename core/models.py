4333a109925b79b0a7dfcaf4c398d0db2a72437b
Jdexurbano <dexterurbano25@gmail.com>
2024-11-04 17:32:09 +0800

    add article model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Article(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name = 'articles')
+    title = models.CharField(max_length = 100)
+    content = models.TextField()
+    published_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return str(f"{self.author} {self.title}")
