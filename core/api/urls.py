085d142644c18bdfa306e95d2df1724929bb7d7d
Jdexurbano <dexterurbano25@gmail.com>
2024-11-04 17:55:00 +0800

    add article list

--- core/api/urls.py
+++ core/api/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('token/',TokenObtainPairView.as_view(), name = 'token_obtain_pair'),
     path('token/refresh/',TokenRefreshView.as_view(), name = 'token_refresh'),
     path('register/',views.UserRegistrationView.as_view(),name = 'register'),
+
+    #articles
+    path('articles/',views.ArticleListView.as_view(),name = 'article_list'),
 ]
--- core/api/views.py
+++ core/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework import status
+from core.models import Article
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
-from .serializers import UserRegistrationSerializer
+from .serializers import UserRegistrationSerializer, ArticleSerializer
 
 
 #class view for user registration
@@ -13,3 +15,13 @@ class UserRegistrationView(APIView):
             serializer.save()
             return Response({"message":"account successfully created"}, status = status.HTTP_201_CREATED)
         return Response(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
+
+
+class ArticleListView(APIView):
+    
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request):
+        article = Article.objects.all()
+        serializer = ArticleSerializer(article, many = True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
